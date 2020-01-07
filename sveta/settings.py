e60e56fe45a72d3a86fb6c06590fb4e82cbd8cd2
KrissLa <arsavit@gmail.com>
2020-01-08 01:31:18 +0300

    6nd

--- sveta/settings.py
+++ sveta/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '#083u#88)^pai8)2*#)1gon_^z5!tp^i81f0=p+df6&6bet5-@'
+# self.SECRET_KEY = os.getenv('SECRET_KEY')
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '^_$sypd3lcotjci0=z*lz1%_#6%)tj(f^pfxw0r&pt9e(pn@mn')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
