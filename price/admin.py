92bfd6e2a298967f6a80e41621a713acaa171aaa
KrissLa <arsavit@gmail.com>
2020-01-23 21:49:48 +0300

    from server

--- price/admin.py
+++ price/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-
+#from django import forms
 from .models import Price, Price_Description, Tariffs
+#from ckeditor.widgets import CKEditorWidget
 
-# Register your models here.
+
+#class TariffsAdminForm(forms.ModelForm):
+#    tariffs_description = forms.CharField(widget=CKEditorWidget())
+#    class Meta:
+#        model = Tariffs
+#        fields = '__all__'
+
 
 class Price_DescriptionInline(admin.StackedInline):
     model = Price_Description
@@ -14,7 +21,7 @@ class TariffsInline(admin.StackedInline):
     model = Tariffs
     max_num = 100
     extra = 1
-
+#    form = TariffsAdminForm
 
 class PriceAdmin(admin.ModelAdmin):
     inlines = [Price_DescriptionInline, TariffsInline, ]
--- sveta/urls.py
+++ sveta/urls.py
@@ -73,9 +73,9 @@ urlpatterns = [
     path('photosessions/love_story_photosessiya/', include('love_story_photosessiya.urls')),
     path('reviews/', include('reviews.urls')),
     path('', include('main.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
-
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+#    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+ #   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
